1e3364c0a6a79281eff402c4bd19ff0641221024
Katelyn Luther <katelynluther87@aim.com>
2023-02-01 17:13:20 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []
+    for (let x = 0; x < names.length; x++) {
+      messages.push("Thank you, " + names[x] + "," + " for the wonderful " + event + " gift!")
+    }
+    return messages;
+  }
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+      console.log(i--);
+    }
+  }
+  
+  countDown(10);
